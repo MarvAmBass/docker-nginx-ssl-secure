28c0d371e483ac2ab10d90610b4ff176129f85f7
Marvin Winkler (Admin) <m@rvin.at>
2014-11-16 13:09:18 +0100

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM nginx
+MAINTAINER MarvAmBass
+
+RUN apt-get update && apt-get install -y \
+    openssl
+
+RUN rm -rf /etc/nginx/conf.d/*
+RUN mkdir -p /etc/nginx/external
+
+ADD ssl.conf /etc/nginx/conf.d/ssl.conf
+ADD security.conf /etc/nginx/conf.d/security.conf
+
+ADD entrypoint.sh /opt/entrypoint.sh
+RUN chmod a+x /opt/entrypoint.sh
+ENTRYPOINT ["/opt/entrypoint.sh"]
+CMD ["nginx", "-g", "daemon off;"]
---
+++ entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cat <<EOF
+Welcome to the marvambass/nginx-ssl-secure container
+
+IMPORTANT:
+  IF you use SSL inside your personal NGINX-config,
+  you should add the Strict-Transport-Security header like:
+
+    add_header Strict-Transport-Security "max-age=31536000; includeSubdomains";
+
+  to your config.
+  After this you should gain a A+ Grade on the Qualys SSL Test
+
+EOF
+
+
+DH4096="/etc/nginx/external/dh4096.pem"
+
+if [ ! -e "$DH4096" ]
+then
+  echo ">> seems like the first start of nginx"
+  echo ">> doing some preparations..."
+  echo ""
+
+#  openssl dhparam -out "$DH4096" 4096
+  openssl dhparam -out "$DH4096" 512
+fi
+
+# exec CMD
+echo ">> exec docker CMD"
+echo "$@"
+"$@"
