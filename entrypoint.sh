6d99a3746222eb41b4d588fc4b27e5e45f89a8dd
Marvin <marvambass@gmail.com>
2014-11-20 18:15:43 +0100

    added self signed cert autocreator

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,6 +33,16 @@ then
   openssl dhparam -out "$DH" $DH_SIZE
 fi
 
+if [ ! -e "/etc/nginx/external/cert.pem" ] || [ ! -e "/etc/nginx/external/key.pem" ]
+then
+  echo ">> generating self signed cert"
+  openssl req -x509 -newkey rsa:4086 \
+  -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=localhost" \
+  -keyout "/etc/nginx/external/key.pem" \
+  -out "/etc/nginx/external/cert.pem" \
+  -days 3650 -nodes -sha256
+fi
+
 echo ">> copy /etc/nginx/external/*.conf files to /etc/nginx/conf.d/"
 cp /etc/nginx/external/*.conf /etc/nginx/conf.d/ 2> /dev/null > /dev/null
 
